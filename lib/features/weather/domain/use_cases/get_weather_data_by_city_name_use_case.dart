41184632524a5ceb4a5aa3f379185824b60e5ae1
mostafa <mustafa.bahr12323@gmail.com>
2023-10-17 15:54:43 +0300

    GetWeatherDataByCityNameUseCase

--- lib/features/weather/domain/use_cases/get_weather_data_by_city_name_use_case.dart
+++ lib/features/weather/domain/use_cases/get_weather_data_by_city_name_use_case.dart
@@ -1,11 +1,16 @@
 import 'package:clean_arch_new_usama_elgendy/core/errors/error.dart';
 import 'package:clean_arch_new_usama_elgendy/core/use_cases/use_case_with_param.dart';
+import 'package:clean_arch_new_usama_elgendy/features/weather/domain/entites/weather_entity.dart';
+import 'package:clean_arch_new_usama_elgendy/features/weather/domain/repos/weather_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class GetWeatherDataByCityNameUseCase extends UseCaseWithParam{
+
+  final WeatherRepo weatherRepo;
+  GetWeatherDataByCityNameUseCase(this.weatherRepo);
+
   @override
-  Future<Either<Errors, dynamic>> call(param) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either<Errors, List<WeatherEntity>>> call(param) async{
+    return await weatherRepo.getWeatherDataByCityName("cityName");
   }
 }
